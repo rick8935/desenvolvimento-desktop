eebd4bbdd145952f5fa7de9c11451553af576506
rick8935 <senac.rickboone@gmail.com>
2025-04-07 21:51:07 -0300

    feat: Adiciona gridview

--- MultiApps/MODEL/MultiApps.Models/Repositories/CategoriaRepository.cs
+++ MultiApps/MODEL/MultiApps.Models/Repositories/CategoriaRepository.cs
@@ -1,26 +1,54 @@
-﻿using Dapper;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
 using MultiApps.Models.Entidades;
 using MySql.Data.MySqlClient;
-using System.Data;
 
-namespace MultiApps.Models.Repositories
+namespace MultApps.Models.Repositories
 {
     public class CategoriaRepository
     {
-        public string connectionString = "Server=localhost;Database=multiapps_dev;Uid=root;Pwd=root";
+        public string ConnectionString = "Server=localhost;Database=multiapps_dev; Uid=root;Pwd=root";
 
-        public bool cadastrarCategoria(Categoria categoria)
+
+        public bool CadastrarCategoria(Categoria categoria)
         {
-            using (IDbConnection db = new MySqlConnection(connectionString))
+            using (IDbConnection db = new MySqlConnection(ConnectionString))
             {
-                var comandoSql = @"INSERT INTO categoria (nome, status) VALUES (@nome, @status)";
+                var comandoSql = @"INSERT INTO categoria (nome, status)
+                                   VALUES(@Nome, @Status )";
 
-                var parametro = new DynamicParameters();
-                parametro.Add("@nome", categoria.Nome);
-                parametro.Add("@status", categoria.Status);
-                            
-                var resultado = db.Execute(comandoSql, parametro);
+                var parametros = new DynamicParameters();
+                parametros.Add("@Nome", categoria.Nome);
+                parametros.Add("@Status", categoria.Status);
+
+                var resultado = db.Execute(comandoSql, parametros);
                 return resultado > 0;
+            }
+        }
+
+        public List<Categoria> ListarTodasCategorias()
+        {
+            using (IDbConnection db = new MySqlConnection(ConnectionString))
+            {
+                var comandoSql = @"SELECT id, nome, data_criacao AS DataCriacao, data_alteracao AS DataAlteracao, status FROM categoria";
+                var resultado = db.Query<Categoria>(comandoSql).ToList();
+                return resultado;
+            }
+        }
+
+        public Categoria ObterCategoria(int id)
+        {
+            using (IDbConnection db = new MySqlConnection(ConnectionString))
+            {
+                var comandoSql = @"SELECT id, nome, data_criacao AS DataCriacao, data_alteracao AS DataAlteracao, status FROM categoria WHERE id = @Id";
+                var parametros = new DynamicParameters();
+                parametros.Add(@"Id", id);
+
+                var resultado = db.Query<Categoria>(comandoSql, parametros).FirstOrDefault();
+                return resultado;   
             }
         }
     }
--- MultiApps/VIEW/MultApps.Windows/FrmCategoria.cs
+++ MultiApps/VIEW/MultApps.Windows/FrmCategoria.cs
@@ -1,6 +1,6 @@
-﻿using MultiApps.Models.Entidades;
+﻿using MultApps.Models.Repositories;
+using MultiApps.Models.Entidades;
 using MultiApps.Models.Enum;
-using MultiApps.Models.Repositories;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -27,7 +27,7 @@ namespace MultApps.Windows
             categoria.Status = (StatusEnum)cmbStatus.SelectedIndex;
 
             var categoriaRepository = new CategoriaRepository();
-            var resultado = categoriaRepository.cadastrarCategoria(categoria);
+            var resultado = categoriaRepository.CadastrarCategoria(categoria);
             if (resultado)
             {
                 MessageBox.Show("Categoria cadastrada com sucesso");
@@ -36,7 +36,122 @@ namespace MultApps.Windows
             {
                 MessageBox.Show("Falha ao cadastrar categoria");
             }
+
+            CarregarTodasCategorias();            
+        }
+        private void CarregarTodasCategorias()
+        {
+            var categoriaRepository = new CategoriaRepository();
+            var listaDeCategorias = categoriaRepository.ListarTodasCategorias();
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.Columns.Clear();
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Id",
+                HeaderText = "Id",
+                MinimumWidth = 100,
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Nome",
+                HeaderText = "Nome da Categoria",
+                MinimumWidth = 300,
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "DataCriacao",
+                HeaderText = "Data de Cadastro",
+                DefaultCellStyle = new DataGridViewCellStyle { Format = "dd/MM/yyyy HH:MM" },
+                MinimumWidth = 200
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "DataAlteracao",
+                HeaderText = "Data de Alteração",
+                DefaultCellStyle = new DataGridViewCellStyle { Format = "dd/MM/yyyy HH:MM" },
+                MinimumWidth = 200
+
+            });
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                DataPropertyName = "Status",
+                HeaderText = "Status",
+                DefaultCellStyle = new DataGridViewCellStyle
+                {
+
+                }
+            });
+
+            dataGridView1.DataSource = listaDeCategorias;
+
+
+            //Depois de preencher será chamado o metodo CellFormating para verificar o status e pintar
+            dataGridView1.CellFormatting += dataGridView1_CellFormatting;
+
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (dataGridView1.Columns[e.ColumnIndex].DataPropertyName == "Status")
+            {
+                if (e.Value != null)
+                {
+                    StatusEnum status = (StatusEnum)e.Value;
+                    switch (status)
+                    {
+                        case StatusEnum.Inativo:
+                            e.CellStyle.ForeColor = Color.Gray;
+                            break;
+                        case StatusEnum.Ativo:
+                            e.CellStyle.ForeColor = Color.Blue;
+                            break;
+                        case StatusEnum.Excluido:
+                            e.CellStyle.ForeColor = Color.Red;
+                            break;
+                    }
+                }
+            }
+        }
+
+        private void btnTesteObterId_Click(object sender, EventArgs e)
+        {
+            CarregarTodasCategorias();
+
+            var categoriaRepository = new CategoriaRepository();
+            var obterCategoria = categoriaRepository.ObterCategoria(2);
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+            {
+                MessageBox.Show($"Houve um erro ao clicar duas vezes sobre o Grid");
+                return;
+            }
+
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
             
+            var categoriaId = (int)row.Cells[0].Value;
+                
+            var categoriaRepository = new CategoriaRepository();
+            var categoria = categoriaRepository.ObterCategoria(categoriaId);
+
+            if (categoria == null)
+            {
+                MessageBox.Show($"Categoria: #{categoriaId} não encontrada");
+                return;
+            }
+
+            txtId.Text = categoriaId.ToString();
+            txtNome.Text = categoria.Nome;
+            cmbStatus.SelectedIndex = (int)categoria.Status;
+            txtDataCadastro.Text = categoria.DataCriacao.ToString("dd/MM/yyyy HH:mm");
+            txtDataAlteracao.Text = categoria.DataAlteracao.ToString("dd/MM/yyyy HH:mm");
         }
     }
 }
